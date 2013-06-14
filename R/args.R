801d5f0f058f199e88a243827d8cc6f7a914c8da
mataddy <mataddy@gmail.com>
2013-06-14 17:28:29 -0500

    fix path bug in args.R

--- R/args.R
+++ R/args.R
@@ -1,6 +1,5 @@
-
-## R arguments
+## R arguments (user specific; yours will be different from mine)
 NC = 16 # number of cores
 LIB = "/scratch/midway/taddy/packages" # lib.loc (use NULL for global)
-EXT = "/scratch/data/taddy/nhlgames" # game records directory
+EXT = "/scratch/midway/taddy/nhlgames" # game records directory
 
