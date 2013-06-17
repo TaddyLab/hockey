fc5d52e3ba5716a5d01eec579f4fcb7ef32358cb
mataddy <taddy@chicagobooth.edu>
2013-06-17 17:17:14 -0500

    unrun code transfer for new nhlscrapr 1.1

--- code/scrape.R
+++ code/scrape.R
@@ -18,47 +18,39 @@ registerDoMC(NC)
 
 allgames <- full.game.database()
 ## you can subset here to scrape fewer seasons
-games <- allgames[as.numeric(allgames$season)>=20022003,]
+games <- allgames[as.numeric(allgames$season)==20022003,][870:900,]
 
-## grab and process games in parallel
+## process games in parallel
 chunk <- ceiling( (0:NC)*(nrow(games)/NC) )
 print(chunk)
 mcproc <- foreach (k=1:NC) %dopar% {
 	G <- games[(chunk[k]+1):chunk[k+1],]
 	for(i in 1:nrow(G)){
-	    tryCatch(item <- process.single.game(
-    	    		G[i,1],
-        		G$gcode[i],
-          		rdata.folder = EXT,
-          		save.to.file=TRUE),
-	    	error = function(e)
-	    		message(paste("WARNING in game",
-	    				G[i,1],G$gcode[i],":",e)))
-	    if (i%%100 == 0) 
-	        message(paste("proc game", i, "of chunk", k))
+	    item <- process.single.game(G[i,1],G$gcode[i],EXT,save.to.file=TRUE)
+	    if (i%%100 == 0) message(paste("proc game", i, "of chunk", k))
 	}
-}
+} 
 warnings()
 
-## build out roster material
+## build out roster material and save
 roster <- construct.rosters(games, rdata.folder = EXT)
 save(roster, file="../data/roster.RData")
-games <- roster$games
 
-## augment the game information
+## extract valid games
+validgames <- roster$games[roster$games$valid,]
+master <- roster$master.list
+
+## augment the game information in parallel
+chunk <- ceiling( (0:NC)*(nrow(validgames)/NC) )
+print(chunk)
 mcaug <- foreach (k=1:NC) %dopar% {
-	G <- games[(chunk[k]+1):chunk[k+1],]
-	for(i in 1:nrow(G))if(G$valid[i]){
-		tryCatch({pl.table <- retrieve.game(G$season[i], G$gcode[i],EXT)
-			  if (length(pl.table$game.record) > 0){ 
-			   	rec <- augment.game(pl.table,roster, 
-				 		G$season[i], G$gcode[i])
-				write.table(rec, file=sprintf("%s/%s-%s-gamerec.txt",
-						EXT,G$season[i], G$gcode[i]), 
-						sep="|", quote=FALSE) }},
-			error = function(e) 
-			      message(paste("WARNING in",G$season[i],G$gcode[i],":",e)))  
-		if (i%%10 == 0) message(paste("augment: game", i, "of chunk ", k))}
+	G <- validgames[(chunk[k]+1):chunk[k+1],]
+	for(i in 1:nrow(G)){
+		rec <- augment.game(retrieve.game(G$season[i], G$gcode[i], EXT),master)
+		write.table(rec, file=sprintf("%s/%s-%s-gamerec.txt",
+					EXT,G$season[i], G$gcode[i]), 
+					sep="|", quote=FALSE)
+		if (i%%100 == 0) message(paste("write game", i, "of chunk ", k))}
 }
 warnings()
 
