7223c3a64c52bfbbeb8a77d8df3b7e9f4ae3c3ff
Robert B. Gramacy <rbgramacy@chicagobooth.edu>
2013-06-27 20:28:48 -0500

    part way through generating Bayes betas

--- code/logistic_fits.R
+++ code/logistic_fits.R
@@ -2,10 +2,20 @@ source("args.R")
 
 load("../data/nhlscrapr_logit_data.RData")
 library(textir, lib=LIB)
+library(reglogit, lib=LIB)
 
+## team+player model with special teams indicators
 penSTP <- c(rep(0,ncol(XS)+ncol(XT)+1), rep(data.frame(c(7.5,1/2)), ncol(XP)))
-fitSTP <- mnlm(counts=Y, covars=cbind(XS, XT,XP), verb=1, penalty=penSTP, normalize=FALSE)
-sum(coef(fitSTP)[-(1:39)]!=0)
+fitSTP <- mnlm(counts=Y, covars=cbind(XS,XT,XP), verb=1, penalty=penSTP, normalize=FALSE)
 
-## save the output, includes the inputs
-save.image("../results/nhlscrapr_logit_fits.RData")
+## player-only model with special teams indicators
+penSP <- c(rep(0,ncol(XS)+1), rep(data.frame(c(7.5,1/2)), ncol(XP)))
+fitSP <- mnlm(counts=Y, covars=cbind(XS,XP), verb=1, penalty=penSP, normalize=FALSE)
+
+## fully Bayesian team+player model with special teams indicators
+S <- 10000
+fitSP.rl <- reglogit(S, Y, cbind(XS,XP), save.latents=FALSE, normalize=FALSE, sparse=TRUE)
+fitSTP.rl <- reglogit(S, Y, cbind(XS, XT, XP), save.latents=FALSE, normalize=FALSE)
+
+## save the output, does not include the inputs
+save(fitSP, fitSTP, fitSP.rl, fitSTP.rl, "../results/logit_fits.RData")
