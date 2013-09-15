e553e13b0408b8303c0ee69d3d59c8c21a853dff
Sen Tian <tiansen0542@gmail.com>
2013-09-15 09:56:01 -0500

    update

--- code/args.R
+++
@@ -1,5 +0,0 @@
-## R arguments (user specific; yours will be different from mine)
-NC = 4#16 # number of cores
-LIB = NULL#"/scratch/midway/taddy/packages" # lib.loc (use NULL for global)
-EXT = "../data/nhlgames"#"/scratch/midway/taddy/nhlgames" # game records directory
-
