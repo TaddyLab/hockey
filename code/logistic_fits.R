c4c6c57d9cf81f2717d7783d8185176e87196c22
Robert B. Gramacy <rbgramacy@chicagobooth.edu>
2013-09-15 21:07:07 -0400

    snow implementation for parallel reglogit

--- code/logistic_fits.R
+++ code/logistic_fits.R
@@ -6,7 +6,6 @@ library(textir)
 library(mvtnorm)
 library(Matrix)
 library(reglogit)
-## library(gputools)
 
 ## team+player model with special teams indicators
 penSTP <- c(rep(0,ncol(XS)+ncol(XT)+1), rep(data.frame(c(7.5,1/2)), ncol(XP)))
@@ -19,13 +18,48 @@ fitSP <- mnlm(counts=Y, covars=cbind(XS,XP), verb=1, penalty=penSP, normalize=FA
 ## save the output, does not include the inputs
 save(fitSP, fitSTP, file="../results/logistic_map_fits.RData")
 
+library(parallel)
+
+reglogit.chunk <- function(chunk, S, Y, XS, XT, XP)
+	{
+		if(chunk == "team-player") {
+			out <- reglogit(S, Y, cbind(XS, XT, XP), save.latents=FALSE, 
+				normalize=FALSE, sparse=TRUE)
+		} else if(chunk == "player") {
+			out <- reglogit(S, Y, cbind(XS,XP), save.latents=FALSE, 
+				normalize=FALSE, sparse=TRUE)
+		} else stop("unknown chunk type")
+
+		return(out)
+	}
+	
+reglogit.snow2 <- function(cls, S, Y, XS, XT, XP)
+	{
+		if(length(cls) != 2) stop("only makes sense to use this function with a 2-clister")
+		chunks <- list("team-player", "player")
+		clusterEvalQ(cls, library(mvtnorm))
+		clusterEvalQ(cls, library(Matrix))
+		clusterEvalQ(cls, library(reglogit))
+		outs <- clusterApply(cls, chunks, reglogit.chunk, S=S, Y=Y, XS=XS, XT=XT, XP=XP)
+	}
+
+## length of MCMC run
+S <- 10000
+
+## do the two reglogits in parallel
+cls <- makeCluster(rep("localhost", 2), type="PSOCK")
+outs <- reglogit.snow2(cls, S, Y, XS, XT, XP)
+stopCluster(cls)
+
 ## fully Bayesian team+player model with special teams indicators
 S <- 10000
-fitSTP.rl <- reglogit(S, Y, cbind(XS, XT, XP), save.latents=FALSE, normalize=FALSE, sparse=TRUE)
+## fitSTP.rl <- reglogit(S, Y, cbind(XS, XT, XP), save.latents=FALSE, normalize=FALSE, sparse=TRUE)
+fitSTP.rl <- outs[[1]]
 ## save the output
 save(fitSP.rl, file="../results/logistic_rl_fits.RData")
 
 ## player-only model
-fitSP.rl <- reglogit(S, Y, cbind(XS,XP), save.latents=FALSE, normalize=FALSE, sparse=TRUE)
+## fitSP.rl <- reglogit(S, Y, cbind(XS,XP), save.latents=FALSE, normalize=FALSE, sparse=TRUE)
+fitSP.rl <- outs[[2]]
 ## save the output
 save(fitSP.rl, fitSTP.rl, file="../results/logistic_rl_fits.RData")
