c509f0268ccca2b3b7ce19e6b32239f76c109c9d
Robert B. Gramacy <rbgramacy@chicagobooth.edu>
2013-10-20 14:57:02 -0400

    code to delete unplayed game files

--- code/scrape.R
+++ code/scrape.R
@@ -46,7 +46,6 @@ registerDoMC(NC)
 ## grab the full game data
 allgames <- full.game.database()
 games <- allgames
-
 grcode <- apply(games[,c("season","gcode")], 1, function(r) paste(r,collapse="."))
 
 ## subset for only valid ones we don't have
@@ -75,6 +74,26 @@ if(ng > 0) {
 	} 
 warnings()
 }
+
+
+## remove downloaded files for unplayed games -- this is 
+## needed until Andrew updates nhlscrapr for new season
+
+## get the 2013-2014 game records
+fs <- list.files(EXT, pattern="20132014-[0-9]*-gamerec.txt")
+games <- strsplit(fs, "-")
+played <- as.numeric(unlist(lapply(games, function(x) x[2])))
+## get 2013-2014 processed.RData files
+fs <- list.files(EXT, pattern="20132014-[0-9]*-processed.RData")
+games <- strsplit(fs, "-")
+all <- as.numeric(unlist(lapply(games, function(x) x[2])))
+## remove those which do not have a gamerec
+unlink(fs[!(all %in% played)])
+## same for raw .RData files
+fs <- list.files(EXT, pattern="20132014-[0-9]*.RData")
+unlink(fs[!(all %in% played)])
+
+## done
 
 ## build out roster material and save (note we use allgames here)
 roster <- construct.rosters(allgames[allgames$valid,], rdata.folder = gamepath)
