3df3a9eae6228cc6433251d1909df3940cd3f1b1
Robert B. Gramacy <rbgramacy@chicagobooth.edu>
2014-01-14 16:31:54 -0600

    more cleanup

--- code/glchart.R
+++ code/glchart.R
@@ -1,9 +1,34 @@
+#*******************************************************************************
+#
+# Chicago Hockey Analytics: Robert B, Gramacy and Matt Taddy
+# Copyright (C) 2013, The University of Chicago
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# Questions? Contact Robert B. Gramacy (rbgramacy@chicagobooth.edu), or
+#                    Matt Taddy (taddy@chicagobooth.edu)
+#
+#*******************************************************************************
+
 ## googlechart:
 ##
 ## create a google chart with the specified data, header, footer,
 ## and output file
 
-URL <- "mtaddy@faculty.chicagobooth.edu:/Faculty/matt.taddy/hockey" 
+## URL <- "mtaddy@faculty.chicagobooth.edu:/Faculty/matt.taddy/hockey" 
+URL <- "rgramacy@faculty.chicagobooth.edu:/Faculty/robert.gramacy/hockey"
 
 googlechart <- function(M, header, footer, outfile)
  {
