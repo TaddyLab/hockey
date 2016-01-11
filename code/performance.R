f594759b999e5b93687a4a62ec01fb56f5c62eb7
mataddy <mataddy@outlook.com>
2016-01-11 11:12:57 -0600

    new org for salary analysis

--- code/performance.R
+++ code/performance.R
@@ -3,8 +3,6 @@
 # The hockey data are built via design.R from games obtained via blog/scrape.R
 # Salaries are taken from 'nhlsalaries_bhz_nc/txt' in the sentian/hockey fork
 
-setwd("/Users/Sen/Documents/hockey_new")
-
 # flags for CORSI/FENWICK/GOAL
 CORSI = FALSE
 FENWICK = FALSE
@@ -156,54 +154,3 @@ write.csv(perf,
     file=sprintf("results/performance-%s.csv",suffix), 
     row.names=FALSE, quote=FALSE)
 
-
-################ Sen added
-#### overall performance of each player
-#### player-special team-team model
-# clean the workspace 
-rm(list=ls())
-
-B_pts.p <- list()
-ppm_pts.p <- list()
-suffix <- c("goals","corsi","fenwick")
-for(i in 1:3){
-  load(sprintf("data/nhldesign-%s.rda",suffix[i]))
-  XT.season = XT
-  teams <- sort(unique(c(goal$hometeam,goal$awayteam)))
-  XT = Matrix(0,ncol=length(teams),nrow=nrow(XT.season),dimnames=list(goal$gcode,teams))
-  tc <- c(goal$hometeam,goal$awayteam)
-  tf <- factor(tc,levels=teams)
-  tw <- matrix(as.numeric(tf),ncol=2)
-  XT[cbind(1:nrow(XT.season),tw[,1])] <- 1
-  XT[cbind(1:nrow(XT.season),tw[,2])] <- -1
-  XT = cbind(XT,"ATL/WPG"=XT[,"ATL"]+XT[,"WPG"])
-  XT = XT[,-c(2,30)]
-  # model fit: team+player+special team
-  fit_pts <- gamlr(cBind(XS,XT,XP), Y, gamma=0, 
-                   standardize=FALSE, verb=1,
-                   family="binomial", free=1:c(ncol(XS)+ncol(XT)))
-  B_pts <- coef(fit_pts)[-1,] # corrected AICc selection
-  # coef of the players
-  B_pts.p[[i]] = B_pts[(ncol(XS)+ncol(XT)+1):length(B_pts)]
-  # ppm
-  ng <- colSums(abs(XP)) 
-  prob <- 1/(1+exp(-B_pts.p[[i]])) 
-  ppm_pts.p[[i]] <- ng*(2*prob-1)
-}
-names(B_pts.p) = suffix
-names(ppm_pts.p) = suffix
-# common players
-common = intersect(intersect(names(B_pts.p[[1]]),names(B_pts.p[[2]])),names(B_pts.p[[3]]))
-for(i in 1:3){
-  B_pts.p[[i]] = B_pts.p[[i]][common]
-  ppm_pts.p[[i]] = ppm_pts.p[[i]][common]
-}
-pos = player[rownames(player) %in% common,]$position
-perf_overall <- data.frame(player=common,position=pos,beta.goals=B_pts.p[["goals"]],beta.corsi=B_pts.p[["corsi"]],
-                          beta.fenwick=B_pts.p[["fenwick"]],ppm.goals=ppm_pts.p[["goals"]],
-                          ppm.corsi=ppm_pts.p[["corsi"]],ppm.fenwick=ppm_pts.p[["fenwick"]])
-# write to file
-rownames(perf_overall) <- NULL
-write.csv(perf_overall, 
-          file="results/performance-overall.csv", 
-          row.names=FALSE, quote=FALSE)
--- code/salary.R
+++ code/salary.R
@@ -1,20 +1,27 @@
 # Copyright (C) 2015, Matt Taddy and Robert B Gramacy and Sen Tian
 # comparison between salary and performance
 
-setwd("/Users/Sen/Documents/hockey_new")
-# flags for CORSI/FENWICK/GOAL
-CORSI = FALSE
-FENWICK = FALSE
-if(CORSI & FENWICK){stop("Multiple flags")}
+library(gamlr)
 
-## grab performance estimates
-if(CORSI){ suffix <- "corsi" 
-} else if(FENWICK){ suffix <- "fenwick" 
-} else{ suffix <- "goals"}
-perf <- read.csv(sprintf("results/performance-%s.csv",suffix))
-rownames(perf) <- paste(perf$player,perf$season,sep="_")
+####################
+## read performances
+perf_goals <- read.csv("results/performance-goals.csv")
+rownames(perf_goals) <- paste(perf_goals$player,perf_goals$season,sep="_")
+perf_corsi <- read.csv("results/performance-corsi.csv")
+rownames(perf_corsi) <- paste(perf_corsi$player,perf_corsi$season,sep="_")
+perf_fenwick <- read.csv("results/performance-fenwick.csv")
+rownames(perf_fenwick) <- paste(perf_fenwick$player,perf_fenwick$season,sep="_")
+
+colnames(perf_corsi) <- paste(colnames(perf_corsi),"corsi",sep=".")
+colnames(perf_fenwick) <- paste(colnames(perf_fenwick),"fenwick",sep=".")
+
+perf <- perf_goals
+perf <- cbind(perf, perf_corsi[match(rownames(perf),rownames(perf_corsi)),-(1:2)])
+perf <- cbind(perf, perf_fenwick[match(rownames(perf),rownames(perf_fenwick)),-(1:2)])
 
 seasons <- levels(factor(perf$season))
+
+##############
 ## read salary
 salary <- read.table("data/nhlsalaries.txt", 
     sep="|", header=TRUE,quote="",as.is=TRUE,row.names=1)
@@ -22,117 +29,47 @@ rownames(salary)[rownames(salary)=="ANDREI_KASTSITSYN"] <- "ANDREI_KOSTITSYN"
 rownames(salary)[rownames(salary)=="P. J._AXELSSON"] <- "P.J._AXELSSON"
 colnames(salary) <- c(seasons,"total")
 
-milperyear <- unlist(salary[,seasons])
-names(milperyear) <- paste(rownames(salary), 
+sal <- unlist(salary[,seasons])
+names(sal) <- paste(rownames(salary), 
                         rep(seasons,each=nrow(salary)),
                         sep="_")
-milperyear <- milperyear[match(rownames(perf),names(milperyear))]
-names(milperyear) <- rownames(perf)
+sal <- sal[match(rownames(perf),names(sal))]
+names(sal) <- rownames(perf)
 
 ## who doesn't have salary? 
-nosal <- which(is.na(milperyear)| milperyear==0)
+nosal <- which(is.na(sal)| sal==0)
 (length(nosal))
 head(perf[nosal,])
 ## remove them from calculations
 perf <- perf[-nosal,]
-milperyear <- milperyear[-nosal]
+sal <- sal[-nosal]
 
-## summarize correlations
-salarycorr <- matrix(nrow=length(seasons)+2, ncol=ncol(perf)-2, 
-    dimnames=list(c("overall","averaged",seasons),names(perf)[-(1:2)]))
-# overall
-salarycorr["overall",] <- cor(perf[,-(1:2)],milperyear)
-# per-season
-for(s in seasons){
-    ps <- perf$season==s
-    salarycorr[s,] <- cor(perf[ps,-(1:2)],milperyear[ps])
-}
-# average salary vs avg perf
-players <- sub("\\_\\d+$","",names(milperyear))
-avgsal <- tapply(milperyear,players,mean)
-perfavg <- aggregate(perf[,-(1:2)],by=list(players),FUN=mean)
-salarycorr["averaged",] <- cor(perfavg[,-1],avgsal)
+################################
+## read-in team involvement rate
+tir <- read.table("data/team_inv_rate.txt",colClasses=c("factor","factor","numeric","factor"),
+  header=TRUE,sep="|")
+tir <- tir[tir$season!="career",]
+tir$playerseason <- factor(paste(tir$player,tir$season,sep="_"))
+majorteam <- sapply(
+  levels(tir$playerseason), 
+  function(w){
+    tirw <- tir[tir$playerseason==w,]
+    teamw <- tirw$team[which.max(tirw$weight)]
+    return(teamw) })
 
+team <- majorteam[match(rownames(perf),names(majorteam))]
+names(team) <- rownames(perf)
+noteam <- which(is.na(team))
+team <- team[-noteam]
+perf <- perf[-noteam,]
+sal <- sal[-noteam]
 
-printsal <- function(metric,xlab){
-  s <- which(perf$season=="20132014")
-  tab <- perf[s,]
-
-  linfit <- lm(log(milperyear[s]) ~ tab[,metric])
-  tab$resid <- linfit$resid
-
-  plot(tab[,metric], log(milperyear[s]),  
-    xlab=xlab, ylab="log( salary in millions ) ", pch=21, bg=8)
-  abline(a=coef(linfit)[1], b=coef(linfit)[2], col=2)
-
-  tab <- tab[order(tab$resid),]
-
-  for(i in 1:20){
-    l <- nrow(tab)-(i-1)  
-    cat( i,
-      sub("_"," ", tab[i,"player"]),
-      exp(tab[i,"resid"]), 
-      l,
-      sub("_"," ", tab[l,"player"]),
-      exp(tab[l,"resid"]),
-        sep="&")
-    cat("\\\\\n")
-  }
-}
-
-pdf("salaryscatter.pdf",width=3,height=8)
-par(mfrow=c(1,1), mai=c(.9,.9,.2,.2))
-#printsal("prob","goals-PFP")
-printsal("ppm","goals-PPM")
-dev.off()
+#################################
+## combine the data together
+nhldf <- cbind(team,sal,perf)
 
 
-printratio <- function(){
-  s <- which(perf$season=="20132014")
-  tab <- perf[s,]
 
-  tab$pfp.ratio <- tab$prob/milperyear[s]
-  tab$ppm.ratio <- tab$ppm/milperyear[s]
 
-  tabpfp <- tab[order(-tab$pfp.ratio),]
-  tabppm <- tab[order(-tab$ppm.ratio),]
 
-  for(i in 1:20){
-    cat( i,
-      #sub("_"," ", tabpfp[i,"player"]),
-      #tabpfp[i,"pfp.ratio"], 
-      sub("_"," ", tabppm[i,"player"]),
-      round(tabppm[i,"ppm.ratio"],2),
-        sep="&")
-    cat("\\\\\n")
-  }
-}
-printratio()
-########## Sen added
-# add the max(ppm,0) and max(pm,0)
-salcorr.pos <- function(metric){
-  ind = perf[,metric]>0
-  salcorr.pos <- c(cor(perf[ind,metric],milperyear[ind]))
-  players <- sub("\\_\\d+$","",names(milperyear[ind]))
-  avgsal <- tapply(milperyear[ind],players,mean)
-  perfavg <- tapply(perf[ind,metric],players,mean)
-  salcorr.pos <- c(salcorr.pos, cor(perfavg,avgsal))
-  for(s in seasons){
-    ps <- perf$season==s & perf[,metric]>0
-    salcorr.pos <- c(salcorr.pos, cor(perf[ps,metric], milperyear[ps]))
-  }
-  return(salcorr.pos)
-}
-# for ppm
-salcorr.posppm.reg <- salcorr.pos("ppm") # regular
-salcorr.posppm.avg <- salcorr.pos("ppm.avg") # overall
-# for pm
-salcorr.pospm.reg <- salcorr.pos("pm") # regular
-salcorr.pospm.avg <- salcorr.pos("pm.avg") # overall
 
-salarycorr <- cbind(salarycorr, pos.ppm=salcorr.posppm.reg, pos.pm=salcorr.pospm.reg,pos.ppm.avg=salcorr.posppm.avg, pos.pm.avg=salcorr.pospm.avg)
-(salarycorr <- round(salarycorr,3))
-
-write.csv(salarycorr, 
-    file=sprintf("results/salarycorr-%s.csv",suffix), 
-    quote=FALSE)
